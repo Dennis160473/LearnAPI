fbb60cdc9abf9d18793cc28577af0d2df9b9836e
Dennis Angermann <dennis.angermann@tn.techstarter.de>
2025-03-27 13:50:12 +0100

    Create Hausaufgabe27032025.js

---
+++ Hausaufgaben/Hausaufgabe27032025.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const pet = [
+  { id: 1, name: "Nassima", city: "Berlin" },
+  { id: 2, name: "Suheib", city: "Frankfurt" },
+  { id: 3, name: "Dustin", city: "Buxtehude" },
+  { id: 4, name: "Alex", city: "Erfurt" },
+  { id: 5, name: "Marcel", city: "Erfurt" },
+];
