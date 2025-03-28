10267f72bd7da343adbab1cf10d5c26c176b0046
Dennis Angermann <dennis.angermann@tn.techstarter.de>
2025-03-28 14:18:12 +0100

    Create NewAPI

--- Hausaufgaben/28032025/index.js
+++ Hausaufgaben/28032025/index.js
@@ -2,3 +2,21 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 app.use(express.json());
+
+function readFile() {
+  const data = fs.readFileSync("movie.json", "utf-8");
+  return JSON.parse(data);
+}
+
+function writeFile(data) {
+  fs.writeFileSync("movie.json", JSON.stringify(data, null, 2));
+}
+
+app.get("/Pets", (req, res) => {
+  const films = readFile();
+  res.json(films);
+});
+
+app.get("/movie.json", (req, res) => {
+  res.send();
+});
