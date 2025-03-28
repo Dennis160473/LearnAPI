e346571cb36992d343a12ea13742e68f3a551d03
Dennis Angermann <dennis.angermann@tn.techstarter.de>
2025-03-28 15:27:08 +0100

    updateindex.js && changemovie.json

--- Hausaufgaben/28032025/index.js
+++ Hausaufgaben/28032025/index.js
@@ -12,11 +12,48 @@ function writeFile(data) {
   fs.writeFileSync("movie.json", JSON.stringify(data, null, 2));
 }
 
-app.get("/Pets", (req, res) => {
-  const films = readFile();
-  res.json(films);
+app.get("/movie", (req, res) => {
+  const movie = readFile();
+  res.json(movie);
 });
 
-app.get("/movie.json", (req, res) => {
-  res.send();
+app.post("/movie", (req, res) => {
+  const movie = readFile();
+  const { title, year, actor } = req.body;
+
+  if (title && year && actor) {
+    const newmovie = {
+      id: movie.length + 1,
+      title: title,
+      year: year,
+      actor: actor,
+    };
+    movie.push(newmovie);
+    writeFile(movie);
+    res.status(201).json(newmovie);
+  } else {
+    res.send("Data Incomplete!");
+  }
 });
+
+app.put("/movie/:id", (req, res) => {
+  const id = req.params.id;
+  const movie = readFile();
+  const newArt = req.body.art;
+
+  const foundfilm = movie.find((change) => change.id == id);
+  foundfilm.art = newArt;
+  res.json(foundfilm);
+  writeFile(movie);
+});
+
+app.delete("/movie/:id", (req, res) => {
+  const id = req.params.id;
+  const movie = readFile();
+  const index = movie.findIndex((insert) => insert.id == id);
+  const deletemovie = movie.splice(index, 1);
+  writeFile(movie);
+  res.json("Delete complete: " + deletemovie[0].title);
+});
+
+app.listen(5050);
